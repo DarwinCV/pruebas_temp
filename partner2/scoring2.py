ba11e841f238a2367b295cbdff88a7daec0fc44b
Darwin Cubi <darling_dc69@hotmail.com>
2023-06-29 20:25:40 -0500

    Cambiar texto del log final

--- partner2/scoring2.py
+++ partner2/scoring2.py
@@ -22,4 +22,5 @@ pipeline.fit(X_train, y_train)
 
 # Paso 5: Evaluar el rendimiento del modelo en el conjunto de prueba
 r2_score = pipeline.score(X_test, y_test)
-print('Coeficiente de determinación (R2):', r2_score)
+print('Coeficiente de correlacion (R2):', r2_score)
+
