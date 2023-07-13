f04d02300ed401a4c3582b6a48c49fd15492b0c3
Darwin Cubi <darling_dc69@hotmail.com>
2023-07-12 22:11:19 -0500

    add rfm package

--- partner1/scoring1.py
+++ partner1/scoring1.py
@@ -1,6 +1,7 @@
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
+import rfm
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
